8d054018a770ba7ee81e7aca8a1080bd9b831463
Olingah <faitholinga07@gmail.com>
2024-01-22 23:57:29 +0300

    updated app.jsx to have my details

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Faith Oling'a",
+  title: "Cloud Architect",
+  email: "faitholinga07@gmail.com",
+  gitHub: "olingah",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
